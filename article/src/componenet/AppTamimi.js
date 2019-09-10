81e5b49d66ca2dec001ec1dc40db0776d7421c52
TahaAlTamimi <altamimitmo@gmail.com>
2019-09-10 06:12:37 +0300

    add nav bar

---
+++ article/src/componenet/AppTamimi.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import NavBar from './NavBar';
+
+export class AppTamimi extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <NavBar/>
+            </div>
+        )
+    }
+}
+
+export default AppTamimi
